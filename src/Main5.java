df7cddf33d6391104ce04905ded49e6935217ca2
ethsteve <ethernalsteve@gmail.com>
2022-09-27 17:27:27 +0300

    Adding the solution of task #5

---
+++ src/Main5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] numbers = scanner.nextLine().split(" ");
+        byte counter = 0;
+
+        for (String number : numbers) {
+            counter += number.length() == 3 && Short.parseShort(number) % 5 == 0 ? 1 : 0;
+        }
+
+        System.out.println(counter >= 2);
+    }
+}
