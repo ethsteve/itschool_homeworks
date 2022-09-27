f29e7b1483c0aa5c4091dc7a5146128ae6215923
ethsteve <ethernalsteve@gmail.com>
2022-09-27 17:36:44 +0300

    Adding the solution of task #6

---
+++ src/Main6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] coords = scanner.nextLine().split(" ");
+        float x = Float.parseFloat(coords[0]);
+        float y = Float.parseFloat(coords[1]);
+
+        System.out.println(x <= 2 - x * x && y >= x && x <= 0 || y <= 2 - x * x && y > 0 && x <= Math.sqrt(2) ? "YES" : "NO");
+    }
+}
