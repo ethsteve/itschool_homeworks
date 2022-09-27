6741abb014d37979008b25c2986a39f60476ed14
ethsteve <ethernalsteve@gmail.com>
2022-09-27 17:16:24 +0300

    Adding the solution of task #4

---
+++ src/Main4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float x = scanner.nextFloat();
+
+        System.out.println(!(x >= -2 && x <= 3 || x >= 6 && x <= 9));
+    }
+}
